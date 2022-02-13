46f8c51700b1befc935e9b8d3badc1e199771a4c
Bisheng Liu <bisheng.liu@abcam.com>
2022-02-13 21:46:23 +0100

    update serverless.ts

--- kafka-consumer/src/functions/kafka-consumer/index.ts
+++ kafka-consumer/src/functions/kafka-consumer/index.ts
@@ -19,7 +19,9 @@ export const kafkaConsumer = {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [event],
   tags: { stage: "kafka-consumer-${self:custom.stage}" },
+  timeout: 300,
   environment: {
+    AWS_REGION: "eu-west-1",
     SCHEMA_REGISTRY_CREDENTIALS_ARN:
       "${file(deploy/config/${self:custom.stage}.yml)}:custom.schemaRegistry.credentials",
   },
