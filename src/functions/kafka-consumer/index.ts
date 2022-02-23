9b1f9538d9c5dfadd8595e1ca282fea15521cec2
Bisheng Liu <bisheng.liu@abcam.com>
2022-02-23 14:42:04 +0100

    fix-kafka-comsumer-handler

--- src/functions/kafka-consumer/index.ts
+++ src/functions/kafka-consumer/index.ts
@@ -9,16 +9,16 @@ export const kafkaConsumerFactory = (name: string) => {
   const kafkaConsumer = {
     tags: { stage: "kafka-consumer-${self:custom.stage}" },
     timeout: 300,
-  };
-  kafkaConsumer["handler"] = `${handlerPath(__dirname)}/handler.main`;
-  kafkaConsumer["events"] = [kafkaConsumerEvent(KafkaEventKey.BIOCHEMICAL)];
-  kafkaConsumer["environment"] = () => ({
-    SNS_TOPIC_ARN: {
-      Ref: rConstants.SNSFifoTopicResource,
+    handler: `${handlerPath(__dirname)}/handler.main`,
+    events: [kafkaConsumerEvent(KafkaEventKey.BIOCHEMICAL)],
+    environment: {
+      SNS_TOPIC_ARN: {
+        Ref: rConstants.SNSFifoTopicResource,
+      },
+      MESSAGE_GROUP_ID: name,
+      SCHEMA_REGISTRY_CREDENTIALS_ARN:
+        "${file(deploy/config/${self:custom.stage}.yml):custom.schemaRegistry.credentials}",
     },
-    MESSAGE_GROUP_ID: name,
-    SCHEMA_REGISTRY_CREDENTIALS_ARN:
-      "${file(deploy/config/${self:custom.stage}.yml):custom.schemaRegistry.credentials}",
-  });
+  };
   return kafkaConsumer;
 };
